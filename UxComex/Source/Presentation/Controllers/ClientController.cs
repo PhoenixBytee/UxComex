99c7589e7c3a07aa3b456eb9308138ae94a1f3c0
PhoenixBytee <venilson2009@hotmail.com>
2023-04-13 21:12:42 -0300

    feat(client): listagem de clientes

---
+++ UxComex/Source/Presentation/Controllers/ClientController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UxComex.Models;
+using UxComex.Source.Domain.Entities;
+using UxComex.Source.Presentation.ViewModels;
+
+namespace UxComex.Source.Presentation.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var clients = new List<ClientViewModel>
+            {
+                new ClientViewModel { Id = 1, Name = "João da Silva", Telephone = "(11) 99999-9999", Cpf = "111.111.111-11", CreatedAt = DateTime.Now, UpdateddAt = DateTime.Now },
+                new ClientViewModel { Id = 2, Name = "Maria Santos", Telephone = "(11) 88888-8888", Cpf = "222.222.222-22", CreatedAt = DateTime.Now, UpdateddAt = DateTime.Now },
+                new ClientViewModel { Id = 3, Name = "Pedro Souza", Telephone = "(11) 77777-7777", Cpf = "333.333.333-33", CreatedAt = DateTime.Now, UpdateddAt = DateTime.Now },
+            };
+
+            return View(clients);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UxComex/Source/Presentation/ViewModels/ClientViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace UxComex.Source.Presentation.ViewModels
+{
+    public class ClientViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Telephone { get; set; }
+        public string Cpf { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateddAt { get; set; }
+    }
+
+}
